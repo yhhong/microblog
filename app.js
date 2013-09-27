83789fb3cc94e281cb461d5143a46f5ffc13a275
yinghuihong <123997654@qq.com>
2013-09-27 16:11:30 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,39 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');//设置页面模板的位置
+app.set('view engine', 'ejs');//设置模板引擎(页面模板+数据=>HTML)可运行在服务端或者客户端(浏览器)，这里运行在服务端
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));//配置了静态文件服务器，因此/stylesheets/style.css 会定向到 app.js 所在目录的子目录中的文件 public/stylesheets/style.css
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);//规定路径为“/”的 GET 请求由 routes.index 函数处理
+app.get('/users', user.list);
+app.get('/hello', routes.hello);
+app.get('/user/:username', function(req, res) { 
+ res.send('user: ' + req.params.username); 
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+	//res.render 的功能是调用模板引擎，并将其产生的页面直接返回给客户端
+  	res.render('index', { title: 'Express' });//调用视图模板 index，传递 title 变量。最终视图模板生成 HTML 页面，返回给浏览器
+};
+exports.hello = function(req, res) { 
+ 	res.send('The time is ' + new Date().toString()); 
+};

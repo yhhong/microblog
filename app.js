c974dd9c311937e96da8060fd81a33a6334e955e
yinghuihong <123997654@qq.com>
2013-09-29 09:45:17 +0800

    add connect-flash mongodb connect-mongo

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ var path = require('path');
 
 var app = express(); 
 
+// 添加会话支持
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings'); 
+
+var flash = require('connect-flash');
+app.use(flash());
+
+app.use(express.cookieParser());// Cookie解析的中间件
+app.use(express.session({secret: settings.cookieSecret,	store: new MongoStore({db: settings.db})}));//提供会话支持	
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');//设置页面模板的位置
@@ -29,56 +39,83 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);//规定路径为“/”的 GET 请求由 routes.index 函数处理
-app.get('/users', user.list);
-app.get('/hello', routes.hello);
-app.get('/user/:username', function(req, res) { 
- res.send('user: ' + req.params.username); 
-});
-app.get('/list', function(req, res) { 
-	res.render('list', { 
-		title: 'List', 
-		items: [1991, 'byvoid', 'express', 'Node.js'],
-		layout: 'template'
-	}); 
-});
+// app.get('/', routes.index);//规定路径为“/”的 GET 请求由 routes.index 函数处理
+// app.get('/users', user.list);
+// app.get('/hello', routes.hello);
+// app.get('/user/:username', function(req, res) { 
+//  res.send('user: ' + req.params.username); 
+// });
+// app.get('/list', function(req, res) { 
+// 	res.render('list', { 
+// 		title: 'List', 
+// 		items: [1991, 'byvoid', 'express', 'Node.js'],
+// 		layout: 'template'
+// 	}); 
+// });
 
 
 
 
-var util = require('util'); 
-app.locals({ 
-	inspect: function(obj) { 
-	return util.inspect(obj, true); 
- } 
-}); 
-// https://github.com/visionmedia/express/wiki/Migrating-from-2.x-to-3.x
-// dynamicHelpers 换成 middleware 中间件
-// app.dynamicHelpers({ 
-// 	headers: function(req, res) { 
-// 	return req.headers; 
+// var util = require('util'); 
+// app.locals({ 
+// 	inspect: function(obj) { 
+// 	return util.inspect(obj, true); 
 //  } 
 // }); 
+// // https://github.com/visionmedia/express/wiki/Migrating-from-2.x-to-3.x
+// // dynamicHelpers 换成 middleware 中间件
+// // app.dynamicHelpers({ 
+// // 	headers: function(req, res) { 
+// // 	return req.headers; 
+// //  } 
+// // }); 
+// app.use(function(req, res, next){
+//   // res.locals.req = req;
+//   res.locals.headers = req.headers;
+//   next();
+// });
+// // app.use(app.router);
+// app.get('/helper', function(req, res) { 
+// 	res.render('helper', { 
+// 	title: 'Helpers' 
+//  }); 
+// }); 
+
+// //路由
+// app.get('/u/:user', routes.user);
+// app.post('/post', routes.post); 
+// app.get('/reg', routes.reg);
+// app.post('/reg', routes.doReg);
+// app.get('/login', routes.login);
+// app.post('/login', routes.doLogin);
+// app.get('/logout', routes.logout); 
+
+
+// app.dynamicHelpers({
+//     user: function(req, res) {
+//         return req.session.user;
+//     },
+//     error: function(req, res) {
+//         var err = req.flash('error');
+//         if (err.length) return err;
+//         else return null;
+//     },
+//     success: function(req, res) {
+//         var succ = req.flash('success');
+//         if (succ.length) return succ;
+//         else return null;
+//     },
+// });
+
 app.use(function(req, res, next){
-  // res.locals.req = req;
-  res.locals.headers = req.headers;
-  next();
+    res.locals.user = req.session.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
 });
-// app.use(app.router);
-app.get('/helper', function(req, res) { 
-	res.render('helper', { 
-	title: 'Helpers' 
- }); 
-}); 
-
-
-
-
-
-
-
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+routes(app);
--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,69 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-	//res.render 的功能是调用模板引擎，并将其产生的页面直接返回给客户端
-  	res.render('index', { title: 'Express' });//调用视图模板 index，传递 title 变量。最终视图模板生成 HTML 页面，返回给浏览器
-};
-exports.hello = function(req, res) { 
- 	res.send('The time is ' + new Date().toString()); 
-};
+// exports.index = function(req, res){
+// 	//res.render 的功能是调用模板引擎，并将其产生的页面直接返回给客户端
+//   	res.render('index', { title: 'Express' });//调用视图模板 index，传递 title 变量。最终视图模板生成 HTML 页面，返回给浏览器
+// };
+// exports.hello = function(req, res) { 
+//  	res.send('The time is ' + new Date().toString()); 
+// };
 
-exports.user = function(req, res) { 
+// exports.user = function(req, res) { 
+// }; 
+// exports.post = function(req, res) { 
+// }; 
+// exports.reg = function(req, res) { 
+// }; 
+// exports.doReg = function(req, res) { 
+// }; 
+// exports.login = function(req, res) { 
+// }; 
+// exports.doLogin = function(req, res) { 
+// }; 
+// exports.logout = function(req, res) { 
+// };
+
+var crypto = require('crypto');
+var User = require('../models/user.js');
+
+module.exports = function(app) {
+	app.get('/',function(req, res) {res.render('index', {title: '首页'});});
+	app.get('/reg',function(req, res) {res.render('reg', {title: '用户注册'});});
+	app.post('/reg',function(req, res) {
+	    //检验用户两次输入的口令是否一致
+	    if (req.body['password-repeat'] != req.body['password']) {
+	        req.flash('error', '两次输入的口令不一致');
+	        return res.redirect('/reg');
+	    }
+	    //生成口令的散列值
+	    var md5 = crypto.createHash('md5');
+	    var password = md5.update(req.body.password).digest('base64');
+	    var newUser = new User({
+	        name: req.body.username,
+	        password: password,
+	    });
+	    //检查用户名是否已经存在
+	    User.get(newUser.name,function(err, user) {
+	        if (user) err = 'Username already exists.';
+	        if (err) {
+	            req.flash('error', err);
+	            return res.redirect('/reg');
+	        }
+	        //如果不存在则新增用户
+	        newUser.save(function(err) {
+	            if (err) {
+	                req.flash('error', err);
+	                return res.redirect('/reg');
+	            }
+	            req.session.user = newUser;
+	            req.flash('success', '注册成功');
+	            res.redirect('/');
+	        });
+	    });
+	});
+
+
+
+	app.get('/login',function(req, res) {res.render('login', {title: '用户注册'});});
 }; 
-exports.post = function(req, res) { 
-}; 
-exports.reg = function(req, res) { 
-}; 
-exports.doReg = function(req, res) { 
-}; 
-exports.login = function(req, res) { 
-}; 
-exports.doLogin = function(req, res) { 
-}; 
-exports.logout = function(req, res) { 
-};
